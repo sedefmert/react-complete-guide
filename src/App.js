fe3e9a468c57ed4680725456da7ec848c3842cfc
sedef <sedefmert44@gmail.com>
2023-03-14 09:49:05 +0100

    First component created . Css applied.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
+import Card from "./components/UI/Card";
 
-function App() {
+
+const App = () =>  {
+  const expenses = [
+    {
+      id: "e1",
+      title: "toilet paper",
+      amount: 94.2,
+      date: new Date(2022, 1, 12),
+    },
+    {
+      id: "e2",
+      title: "car insurance",
+      amount: 450.2,
+      date: new Date(2022, 4, 4),
+    },
+    {
+      id: "e3",
+      title: "new phone",
+      amount: 700.25,
+      date: new Date(2022, 3, 5),
+    },
+    {
+      id: "e4",
+      title: "new desk",
+      amount: 120.5,
+      date: new Date(2022, 6, 14),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card className="title"><h2>Expenses</h2></Card>
+      <Expenses items={expenses}/>
+      
     </div>
   );
 }
